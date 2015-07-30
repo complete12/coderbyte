69dd0546a8fa0ca69e6abf24043b0580efbe0990
complete12 <complete@hanmail.net>
2015-07-30 15:25:21 +0900

    Create FirstFactorial.rb

---
+++ FirstFactorial.rb
@@ -0,0 +1,33 @@
+def FirstFactorial(num)
+
+  # code goes here
+  if(num > 1)
+    return num * FirstFactorial(num-1)
+  else
+  return num 
+  end
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+FirstFactorial(STDIN.gets)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
