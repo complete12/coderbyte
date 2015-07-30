53caca9610d122cb22b96027d7b1f58c5f5ca910
complete12 <complete@hanmail.net>
2015-07-30 15:50:25 +0900

    Create LetterCapitalize.rb

---
+++ LetterCapitalize.rb
@@ -0,0 +1,34 @@
+def LetterCapitalize(str)
+
+  # code goes here
+   str = str.scan(/\w+/)
+    
+  str.each do |word|
+    word.capitalize!
+  end
+              
+  return str.join(" ")       
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+LetterCapitalize(STDIN.gets)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
