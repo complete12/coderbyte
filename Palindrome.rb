f20e657914b932f0e47f4d1d7e32fe5bf89368dc
complete12 <complete@hanmail.net>
2015-07-30 16:25:37 +0900

    Create Palindrome.rb

---
+++ Palindrome.rb
@@ -0,0 +1,29 @@
+def Palindrome(str)
+
+  # code goes here
+ (str = str.gsub(/\s+/, "")).reverse == str ? "true" : "false"
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+Palindrome(STDIN.gets)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
