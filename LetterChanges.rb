cb4d7a321070918b033267ad533d4aa809d4af4e
complete12 <complete@hanmail.net>
2015-07-30 15:39:06 +0900

    Create LetterChanges.rb

---
+++ LetterChanges.rb
@@ -0,0 +1,35 @@
+def LetterChanges(str)
+
+  # code goes here
+   str = str.split("")
+    
+  str.each do |c|
+    c.next! if c =~ /[a-z]/
+    c.upcase! if c =~ /[aeiou]/
+  end
+                  
+  return str.join
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+LetterChanges(STDIN.gets)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
